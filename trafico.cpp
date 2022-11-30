907186ca8664acd1fdda24760c917c152c276799
Carlos Herrera Vázquez <84268470+Lentejas1@users.noreply.github.com>
2022-11-30 16:43:18 +0100

    .

--- trafico.cpp
+++ trafico.cpp
@@ -2,7 +2,31 @@
 #include <iostream>
 
 using namespace ::std;
+#define N 2000 /* N es el número de pasos*/
+
+double m = 1500, L = 4, C = 20000, v = 120 / 3.6;
+double d = 26, t_0 = C / (m * d);
+double dt = 1. / N;
+double X[N][5]; /* Fila tiempo, columna coche*/
+double V[N][5]; /* Fila tiempo, columna coche*/
+
+double k_func(int j, int k){
+    if (j = 1){
+        return v[]
+    }
+}
+
+double rk4(){
+    for (int j = 0; j < N; j++) {
+        for (int i = 0; i < 4; i++){
+            K[i][j] =
+        }
+    }
+}
 
 int main(){
+    double t_n, tau_n;
+    double k[4][N], l[4][N]; /*Filas son coches y columans pasos de tiempo*/
+
 return 0;
 }
