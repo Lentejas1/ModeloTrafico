1bffac46287c7a1d56e579744ac0aee2bc855696
Carlos Herrera Vázquez <84268470+Lentejas1@users.noreply.github.com>
2022-12-28 21:10:33 +0100

    .

--- graficos.py
+++ graficos.py
@@ -2,102 +2,80 @@ import matplotlib.pyplot as plt
 from matplotlib import animation
 import numpy as np
 import pandas as pd
+
 plt.style.use("science")
 
 N = 2000
-path = "C:/Users/minih/Documents/GitHub/ModeloTrafico/cmake-build-debug/Instantaneo.csv"
-
-plt.figure(figsize=(6, 4))
-df = pd.read_csv(path)
-time = df["t (s)"]
-x_0 = df[" x_0 (m)"]
-x_1 = df[" x_1 (m)"]
-x_2 = df[" x_2 (m)"]
-x_3 = df[" x_3 (m)"]
-x_4 = df[" x_4 (m)"]
-
-plt.scatter(time[0], x_0[0], color="red")
-plt.scatter(time[0], x_1[0], color="orange")
-plt.scatter(time[0], x_2[0], color="gold")
-plt.scatter(time[0], x_3[0], color="green")
-plt.scatter(time[0], x_4[0], color="skyblue")
-
-plt.scatter(time[N - 1], x_0[N - 1], color="red")
-plt.scatter(time[N - 1], x_1[N - 1], color="orange")
-plt.scatter(time[N - 1], x_2[N - 1], color="gold")
-plt.scatter(time[N - 1], x_3[N - 1], color="green")
-plt.scatter(time[N - 1], x_4[N - 1], color="skyblue")
-
-plt.plot(time, x_0, color="red")
-plt.plot(time, x_1, color="orange")
-plt.plot(time, x_2, color="gold")
-plt.plot(time, x_3, color="green")
-plt.plot(time, x_4, color="skyblue")
-
-plt.xlabel("$t$ (s)")
-plt.ylabel("$x$ (m)")
-
-plt.show()
-
-"""fig, axes = plt.subplots()
-graficar0, = plt.scatter([], [])
-graficar1, = plt.scatter([], [])
-graficar2, = plt.scatter([], [])
-graficar3, = plt.scatter([], [])
-graficar4, = plt.scatter([], [])
-
-Re = 0
-Im = 1
-
-def funcion():
-    graficar0.set_data(time, x_0)
-    graficar1.set_data(time, x_1)
-    graficar2.set_data(time, x_2)
-    graficar3.set_data(time, x_3)
-    graficar4.set_data(time, x_4)
-
-    return graficar0, graficar1, graficar3, graficar2, graficar4
 
 
-animation.FuncAnimation(fig, funcion, blit=True)"""
-"""# First set up the figure, the axis, and the plot element we want to animate
-fig = plt.figure()
-ax = plt.axes(xlim=(0, 2), ylim=(-2, 2))
-line, = ax.plot([], [], lw=2)
+def plots(caso):
+    path_x = f"C:/Users/minih/Documents/GitHub/ModeloTrafico/cmake-build-debug/Instantaneo{caso}.csv"
+    path_v = f"C:/Users/minih/Documents/GitHub/ModeloTrafico/cmake-build-debug/Velocidades{caso}.csv"
 
+    plt.figure(figsize=(6, 4))
+    positions = pd.read_csv(path_x)
+    speeds = pd.read_csv(path_v)
 
-# initialization function: plot the background of each frame
-def init():
-    line.set_data([], [])
-    line.set_data([], [])
-    line.set_data([], [])
-    line.set_data([], [])
-    line.set_data([], [])
+    time = positions["t (s)"]
+    x_0 = positions[" x_0 (m)"]
+    x_1 = positions[" x_1 (m)"]
+    x_2 = positions[" x_2 (m)"]
+    x_3 = positions[" x_3 (m)"]
+    x_4 = positions[" x_4 (m)"]
 
-    return line,
+    v_0 = speeds[" v_0 (m/s)"]
+    v_1 = speeds[" v_1 (m/s)"]
+    v_2 = speeds[" v_2 (m/s)"]
+    v_3 = speeds[" v_3 (m/s)"]
+    v_4 = speeds[" v_4 (m/s)"]
 
+    plt.scatter(time[0], x_0[0], color="red", label="Car 0")
+    plt.scatter(time[0], x_1[0], color="orange", label="Car 1")
+    plt.scatter(time[0], x_2[0], color="gold", label="Car 2")
+    plt.scatter(time[0], x_3[0], color="green", label="Car 3")
+    plt.scatter(time[0], x_4[0], color="skyblue", label="Car 4")
 
-# animation function.  This is called sequentially
-def animate(i):
-    line.set_data(time[i], x_0[i])
-    line1.set_data(time[i], x_1[i])
-    line2.set_data(time[i], x_2[i])
-    line.set_data(time[i], x_3[i])
-    line.set_data(time[i], x_4[i])
+    plt.scatter(time[N - 1], x_0[N - 1], color="red")
+    plt.scatter(time[N - 1], x_1[N - 1], color="orange")
+    plt.scatter(time[N - 1], x_2[N - 1], color="gold")
+    plt.scatter(time[N - 1], x_3[N - 1], color="green")
+    plt.scatter(time[N - 1], x_4[N - 1], color="skyblue")
 
-    return line,
+    plt.plot(time, x_0, color="red")
+    plt.plot(time, x_1, color="orange")
+    plt.plot(time, x_2, color="gold")
+    plt.plot(time, x_3, color="green")
+    plt.plot(time, x_4, color="skyblue")
 
+    plt.xlabel("$t$ (s)")
+    plt.ylabel("$x$ (m)")
+    plt.legend()
+    plt.show()
 
-# call the animator.  blit=True means only re-draw the parts that have changed.
-anim = animation.FuncAnimation(fig, animate, init_func=init,
-                               frames=200, interval=20, blit=True)
+    plt.figure(figsize=(6, 4))
 
-# save the animation as an mp4.  This requires ffmpeg or mencoder to be
-# installed.  The extra_args ensure that the x264 codec is used, so that
-# the video can be embedded in html5.  You may need to adjust this for
-# your system: for more information, see
-# http://matplotlib.sourceforge.net/api/animation_api.html
-anim.save('basic_animation.mp4', fps=4, extra_args=['-vcodec', 'libx264'])
+    plt.scatter(time[0], v_0[0], color="red", label="Car 0")
+    plt.scatter(time[0], v_1[0], color="orange", label="Car 1")
+    plt.scatter(time[0], v_2[0], color="gold", label="Car 2")
+    plt.scatter(time[0], v_3[0], color="green", label="Car 3")
+    plt.scatter(time[0], v_4[0], color="skyblue", label="Car 4")
 
-plt.show()
-"""
+    plt.scatter(time[N - 1], v_0[N - 1], color="red")
+    plt.scatter(time[N - 1], v_1[N - 1], color="orange")
+    plt.scatter(time[N - 1], v_2[N - 1], color="gold")
+    plt.scatter(time[N - 1], v_3[N - 1], color="green")
+    plt.scatter(time[N - 1], v_4[N - 1], color="skyblue")
+
+    plt.plot(time, v_0, color="red")
+    plt.plot(time, v_1, color="orange")
+    plt.plot(time, v_2, color="gold")
+    plt.plot(time, v_3, color="green")
+    plt.plot(time, v_4, color="skyblue")
+
+    plt.xlabel("$t$ (s)")
+    plt.ylabel("$v$ (m/s)")
+    plt.legend()
+    plt.show()
+
+plots(1)
+plots(2)
--- trafico.cpp
+++ trafico.cpp
@@ -13,28 +13,29 @@ double t_c= 2 * t_0;
 void respuesta_instantanea(){
     double X[N][5]; /* Fila tiempo, columna coche*/
     double V[N][5]; /* Fila tiempo, columna coche*/
-    int i_norm = int(t_c / dt);
+    int i_col = int(t_c / dt);
+    double v_norm = v * m / C;
+    //cout << v_norm;
+    V[0][0] = v_norm;
+    X[0][0] = 0;
+
+    for (int j = 1; j < 5; j++){
+        X[0][j] = X[0][j-1] - (l+d) / d;
+        V[0][j] = v_norm;
+    }
+
     for (int i=0; i<N-1; i++){ // He cambiado el número de pasos a tres para no petar la consola
         double K[5][4], L[5][4]; /*Filas son coches y columnas K sub algo*/
-        double v_norm = v * m / C;
-        //cout << v_norm;
-        V[0][0] = v_norm;
-        X[0][0] = 0;
-        if (i <= i_norm){
+
+        // Coche 0
+        if (i <= i_col){
             X[i+1][0] = v_norm * (i + 1) * dt, V[i + 1][0] = v_norm;
         }
         else {
             X[i+1][0] = X[i][0] + 0.2 * v_norm * dt, V[i + 1][0] = 0.2 * v_norm;
         }
-        X[0][0]=0;
-        for (int j = 1; j < 5; j++){
-            X[0][j] = X[0][j-1] - (l+d) / d;
-            V[0][j] = v_norm;
-        }
 
-
-
-        K[0][0] = v_norm, K[0][1] = 0 + dt / 2 * K[0][0]; // Hay que restar un índice
+        K[0][0] = 0, K[0][1] = 0 + dt / 2 * K[0][0]; // Hay que restar un índice
         K[0][2] = 0 + dt / 2 * K[0][1], K[0][3] = dt * K[0][2];
 
         L[0][0] = v_norm, L[0][1] = v_norm + dt / 2 * K[0][0];
@@ -48,13 +49,14 @@ void respuesta_instantanea(){
         for (int j=1; j<5; j++){
             K[j][0] = -(V[i][j] - V[i][j-1])/(fabs(X[i][j] - X[i][j-1]));
             L[j][0] = V[i][j];
-            K[j][1] = -((V[i][j] + dt*0.5*K[j][0])- (V[i][j-1] + dt*0.5*K[j-1][0]))/(fabs((X[i][j] + dt*0.5*L[j][0]) - (X[i][j-1] + dt*0.5*L[j-1][0])));
+
+            K[j][1] = -((V[i][j] + dt*0.5*K[j][0]) - (V[i][j-1] + dt*0.5*K[j-1][0]))/(fabs((X[i][j] + dt*0.5*L[j][0]) - (X[i][j-1] + dt*0.5*L[j-1][0])));
             L[j][1] = V[i][j] + dt*0.5*K[j][0];
 
             K[j][2] = -((V[i][j] + dt*0.5*K[j][1])- (V[i][j-1] + dt*0.5*K[j-1][1]))/(fabs((X[i][j] + dt*0.5*L[j][1]) - (X[i][j-1] + dt*0.5*L[j-1][1])));
             L[j][2] = V[i][j] + dt*0.5*K[j][1];
 
-            K[j][3] = -((V[i][j] + dt*K[j][2])- (V[i][j-1] + dt*K[j-1][2]))/(fabs((X[i][j] + dt*L[j][2]) - (X[i][j-1] + dt*L[j-1][2])));
+            K[j][3] = -((V[i][j] + dt*K[j][2]) - (V[i][j-1] + dt*K[j-1][2]))/(fabs((X[i][j] + dt*L[j][2]) - (X[i][j-1] + dt*L[j-1][2])));
             L[j][3] = V[i][j] + dt*K[j][2];
         }
 
@@ -75,7 +77,7 @@ void respuesta_instantanea(){
     }
 
     FILE* data;
-    data = fopen("Instantaneo.csv", "w");
+    data = fopen("Instantaneo1.csv", "w");
     fprintf(data, "t (s), x_0 (m), x_1 (m), x_2 (m), x_3 (m), x_4 (m)\n");
 
     for (int i = 0; i < N - 1; i++){
@@ -84,9 +86,8 @@ void respuesta_instantanea(){
     fprintf(data, "%lf, %lf, %lf, %lf, %lf, %lf", dt*(N-1)/t_0, X[N-1][0]*d,X[N-1][1]*d,X[N-1][2]*d,X[N-1][3]*d,X[N-1][4]*d);
 
     FILE* data2;
-    data2 = fopen("Velocidades.csv", "w");
+    data2 = fopen("Velocidades1.csv", "w");
     fprintf(data2, "t (s), v_0 (m/s), v_1 (m/s), v_2 (m/s), v_3 (m/s), v_4 (m/s)\n");
-    double v_norm = v * m / C;
     for (int i = 0; i < N - 1; i++){
         fprintf(data2, "%lf, %lf, %lf, %lf, %lf, %lf\n", dt*i/t_0, V[i][0]* C/m,V[i][1]* C/m,V[i][2]* C/m,V[i][3]* C/m,V[i][4]* C/m);
     }
@@ -94,51 +95,53 @@ void respuesta_instantanea(){
 
 }
 
-void respuesta_real(int i_reaccion){
+void respuesta_exponencial(){
     double X[N][5]; /* Fila tiempo, columna coche*/
     double V[N][5]; /* Fila tiempo, columna coche*/
-    int i_norm = int(t_c / dt);
+    int i_col = int(t_c / dt);
+    double v_norm = v * m / C;
+    //cout << v_norm;
+    V[0][0] = v_norm;
+    X[0][0] = 0;
+    double t_a = t_0;
+    for (int j = 1; j < 5; j++){
+        X[0][j] = X[0][j-1] - (l+d) / d;
+        V[0][j] = v_norm;
+    }
+
     for (int i=0; i<N-1; i++){ // He cambiado el número de pasos a tres para no petar la consola
         double K[5][4], L[5][4]; /*Filas son coches y columnas K sub algo*/
-        double v_i = v * m / C;
-        //cout << v_i;
-        V[0][0] = v_i;
-        X[0][0] = v_i * i * dt;
-        if (i <= i_norm){
-            X[i+1][0] = v_i * (i + 1) * dt, V[i + 1][0] = v_i;
+
+        // Coche 0
+        if (i <= i_col){
+            X[i+1][0] = v_norm * (i + 1) * dt, V[i + 1][0] = v_norm;
         }
         else {
-            X[i+1][0] = X[i][0] + 0.2 * v_i * dt, V[i + 1][0] = 0.2 * v_i;
+            X[i+1][0] = X[i][0] + 0.2 * v_norm * dt, V[i + 1][0] = v_norm * (1-(i*dt-t_c)/t_a*pow(M_E,(1-(i*dt-t_c)/t_a)));
         }
-        X[0][0]=0;
-        for (int j = 1; j < 5; j++){
-            X[0][j] = X[0][j-1] - (l+d) / d;
-            V[0][j] = v_i;
-        }
-
-
 
         K[0][0] = 0, K[0][1] = 0 + dt / 2 * K[0][0]; // Hay que restar un índice
-        K[0][2] = 0 + dt / 2 * K[0][1], K[0][3] = 0 +  dt * K[0][2];
+        K[0][2] = 0 + dt / 2 * K[0][1], K[0][3] = dt * K[0][2];
 
-        L[0][0] = v_i, L[0][1] = v_i + dt / 2 * K[0][0];
-        L[0][2] = v_i + dt / 2 * K[0][1], L[0][3] = v_i + dt * K[0][2];
-        cout << "Timestep: " << i << "\n" << endl;
+        L[0][0] = v_norm, L[0][1] = v_norm + dt / 2 * K[0][0];
+        L[0][2] = v_norm + dt / 2 * K[0][1], L[0][3] = v_norm + dt * K[0][2];
+        /*cout << "Timestep: " << i << "\n" << endl;
         cout << K[0][0] << "\t" << L[0][0] << "\n" << endl;
         cout << K[0][1] << "\t" << L[0][1] << "\n" << endl;
         cout << K[0][2] << "\t" << L[0][2] << "\n" << endl;
-        cout << K[0][3] << "\t" << L[0][3] << "\n" << endl;
+        cout << K[0][3] << "\t" << L[0][3] << "\n" << endl;*/
 
         for (int j=1; j<5; j++){
             K[j][0] = -(V[i][j] - V[i][j-1])/(fabs(X[i][j] - X[i][j-1]));
             L[j][0] = V[i][j];
-            K[j][1] = -((V[i][j] + dt*0.5*K[j][0])- (V[i][j-1] + dt*0.5*K[j-1][0]))/(fabs((X[i][j] + dt*0.5*L[j][0]) - (X[i][j-1] + dt*0.5*L[j-1][0])));
+
+            K[j][1] = -((V[i][j] + dt*0.5*K[j][0]) - (V[i][j-1] + dt*0.5*K[j-1][0]))/(fabs((X[i][j] + dt*0.5*L[j][0]) - (X[i][j-1] + dt*0.5*L[j-1][0])));
             L[j][1] = V[i][j] + dt*0.5*K[j][0];
 
             K[j][2] = -((V[i][j] + dt*0.5*K[j][1])- (V[i][j-1] + dt*0.5*K[j-1][1]))/(fabs((X[i][j] + dt*0.5*L[j][1]) - (X[i][j-1] + dt*0.5*L[j-1][1])));
             L[j][2] = V[i][j] + dt*0.5*K[j][1];
 
-            K[j][3] = -((V[i][j] + dt*K[j][2])- (V[i][j-1] + dt*K[j-1][2]))/(fabs((X[i][j] + dt*L[j][2]) - (X[i][j-1] + dt*L[j-1][2])));
+            K[j][3] = -((V[i][j] + dt*K[j][2]) - (V[i][j-1] + dt*K[j-1][2]))/(fabs((X[i][j] + dt*L[j][2]) - (X[i][j-1] + dt*L[j-1][2])));
             L[j][3] = V[i][j] + dt*K[j][2];
         }
 
@@ -155,11 +158,11 @@ void respuesta_real(int i_reaccion){
             X[i+1][k] = X[i][k] + dt / 6 * (L[k][0] + 2*L[k][1] + 2*L[k][2]+L[k][3]);
             V[i+1][k] = V[i][k] + dt / 6 * (K[k][0] + 2*K[k][1] + 2*K[k][2]+K[k][3]);
         }
-        cout << "Velocidad coche 1:" << V[i+1][1] << "\n" << endl;
+        //cout << "Velocidad coche 1:" << V[i+1][1] << "\n" << endl;
     }
 
     FILE* data;
-    data = fopen("Instantaneo.csv", "w");
+    data = fopen("Instantaneo2.csv", "w");
     fprintf(data, "t (s), x_0 (m), x_1 (m), x_2 (m), x_3 (m), x_4 (m)\n");
 
     for (int i = 0; i < N - 1; i++){
@@ -168,18 +171,21 @@ void respuesta_real(int i_reaccion){
     fprintf(data, "%lf, %lf, %lf, %lf, %lf, %lf", dt*(N-1)/t_0, X[N-1][0]*d,X[N-1][1]*d,X[N-1][2]*d,X[N-1][3]*d,X[N-1][4]*d);
 
     FILE* data2;
-    data2 = fopen("Velocidades.csv", "w");
+    data2 = fopen("Velocidades2.csv", "w");
     fprintf(data2, "t (s), v_0 (m/s), v_1 (m/s), v_2 (m/s), v_3 (m/s), v_4 (m/s)\n");
-    double v_norm = v * m / C;
     for (int i = 0; i < N - 1; i++){
         fprintf(data2, "%lf, %lf, %lf, %lf, %lf, %lf\n", dt*i/t_0, V[i][0]* C/m,V[i][1]* C/m,V[i][2]* C/m,V[i][3]* C/m,V[i][4]* C/m);
     }
     fprintf(data2, "%lf, %lf, %lf, %lf, %lf, %lf", dt*(N-1)/t_0, V[N-1][0]* C/m,V[N-1][1]* C/m,V[N-1][2]* C/m,V[N-1][3]* C/m,V[N-1][4]* C/m);
 
 }
+
+
 int main(){
     //cout << N * t_0;
 
     respuesta_instantanea();
-return 0;
+    respuesta_exponencial();
+
+    return 0;
 }
